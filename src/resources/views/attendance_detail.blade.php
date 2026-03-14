f80369022aa5d5f558cabfcf04153ed6ccc7f215
yutaka <yu.tama.5@icloud.com>
2026-03-15 06:35:42 +0900

    詳細画面レイアウト完了

--- src/resources/views/attendance_detail.blade.php
+++ src/resources/views/attendance_detail.blade.php
@@ -15,14 +15,18 @@
             <table class="list-form__table">
                 <tr class="list-form__row">
                     <th class="list-form__label">名前</th>
-                    <td class="list-form__data">西　玲奈</td>
+                    <td class="list-form__data">
+                        <div class="list-form__data-container">
+                            <span class="form-container" style="border: none;">西　玲奈</span>
+                        </div>
+                    </td>
                 </tr>
                 <tr class="list-form__row">
                     <th class="list-form__label">日付</th>
                     <td class="list-form__data">
                         <div class="list-form__data-container">
-                            <span>年</span>
-                            <span>月</span>
+                            <span class="form-container" style="border: none;">2026年</span>
+                            <span class="form-container" style="border: none;">03月15日</span>
                         </div>
                     </td>
                 </tr>
@@ -30,9 +34,9 @@
                     <th class="list-form__label">出勤・退勤</th>
                     <td class="list-form__data">
                         <div class="list-form__data-container">
-                            <span>出勤時間が入る</span>
+                            <span class="form-container">出勤時間が入る</span>
                             <span class="separator">～</span>
-                            <span>退勤時間が入る</span>
+                            <span class="form-container">退勤時間が入る</span>
                         </div>
                     </td>
                 </tr>
@@ -40,9 +44,9 @@
                     <th class="list-form__label">休憩</th>
                     <td class="list-form__data">
                         <div class="list-form__data-container">
-                            <span>休憩入</span>
+                            <span class="form-container">休憩入</span>
                             <span class="separator">～</span>
-                            <span>休憩戻</span>
+                            <span class="form-container">休憩戻</span>
                         </div>
                     </td>
                 </tr>
@@ -50,17 +54,26 @@
                     <th class="list-form__label">休憩2</th>
                     <td class="list-form__data">
                         <div class="list-form__data-container">
-                            <span>休憩入2</span>
+                            <span class="form-container">休憩入2</span>
                             <span class="separator">～</span>
-                            <span>休憩戻2</span>
+                            <span class="form-container">休憩戻2</span>
                         </div>
                     </td>
                 </tr>
                 <tr class="list-form__row">
                     <th class="list-form__label">備考</th>
-                    <th class="list-form__border"></th>
+                    <td class="list-form__data">
+                        <div class="list-form__data-container">
+                            <div class="list-form__data-wrapper">
+                                <span class="remarks-column"></span>
+                            </div>
+                        </div>
+                    </td>
                 </tr>
             </table>
+            <div class="correction-btn">
+                <button class="correction">修正</button>
+            </div>
         </div>
     </div>
 </div>
