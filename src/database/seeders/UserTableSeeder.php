f4f3c1796884989d1816000c3b0278635f941d3e
yutaka <yu.tama.5@icloud.com>
2026-04-02 09:07:47 +0900

    ダミーデータ作成完了

--- src/database/seeders/UserTableSeeder.php
+++ src/database/seeders/UserTableSeeder.php
@@ -16,6 +16,45 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        User::Factory()->count(10)->has(Attendance::Factory()->count(1))->create();
+        User::factory()->count(10)->create()->each(function ($user) {
+            $count = 0;
+
+            for ($i = 0; $count < 20 && $i < 30; $i++) {
+
+            $date = now()->subDays($i);
+
+                if ($date->isWeekend()) continue;
+
+                // 1:出勤 (9:00)
+                \App\Models\Attendance::create([
+                    'user_id' => $user->id,
+                    'work_action_id' => 1,
+                    'created_at' => $date->copy()->hour(9)->minute(rand(0, 30)),
+                ]);
+
+                // 3:休憩入 (12:00)
+                \App\Models\Attendance::create([
+                    'user_id' => $user->id,
+                    'work_action_id' => 3,
+                    'created_at' => $date->copy()->hour(12)->minute(rand(0, 60)),
+                ]);
+
+                // 4:休憩戻 (13:00)
+                \App\Models\Attendance::create([
+                    'user_id' => $user->id,
+                    'work_action_id' => 4,
+                    'created_at' => $date->copy()->hour(13)->minute(rand(0, 60)),
+                ]);
+
+                // 2:退勤 (18:00)
+                \App\Models\Attendance::create([
+                    'user_id' => $user->id,
+                    'work_action_id' => 2,
+                    'created_at' => $date->copy()->hour(18)->minute(rand(0, 30)),
+                ]);
+
+                $count++;
+            }
+        });
     }
 }
